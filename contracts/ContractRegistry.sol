92562f75dbe8c4da2837cccf08ea9948a0b265f3
zclod <claudio.zan@outlook.com>
2016-07-05 12:28:58 +0200

    primo commit

---
+++ contracts/ContractRegistry.sol
@@ -0,0 +1,33 @@
+contract ContractRegistry{
+
+
+    struct StorageContract {
+        address owner;
+        address farmer;
+        uint expireDate;
+        bytes32 ipfsAddress;
+
+        // possibile bloccare i fondi di pagamento
+        // direttamente nel contratto?
+        // uint amount;
+    }
+
+
+    uint numContracts;
+    mapping(address => uint[]) contractIndex;
+
+    mapping(uint => StorageContract) public contracts;
+
+
+    function getContracts(address owner) constant returns (uint[] contractList){
+        return contractIndex[owner];
+    }
+
+    function newContract(address owner, address farmer, uint expireDate, bytes32 ipfsAddress) returns (uint contractID){
+        contractID = numContracts++;
+        contracts[contractID] = StorageContract(owner, farmer, expireDate, ipfsAddress);
+
+        contractIndex[owner].push(contractID);
+        contractIndex[farmer].push(contractID);
+    }
+}
---
+++ test/contractregistry.js
@@ -0,0 +1,27 @@
+contract('ContractRegistry', function(accounts) {
+    it("should create 2 storage contracts", function(done) {
+        var reg = ContractRegistry.deployed();
+
+        reg.newContract(accounts[0],accounts[1], 10, "ciao1");
+        reg.newContract(accounts[0],accounts[2], 10, "ciao2");
+        reg.getContracts.call(accounts[0]).then(function(clist) {
+            console.log(clist);
+            assert.equal(clist.length, 2, "expected 2 contracts");
+        }).then(done).catch(done);
+    });
+    it("should retrive the correct contract", function(done){
+        var reg = ContractRegistry.deployed();
+
+        reg.getContracts.call(accounts[0]).then(function(clist) {
+            return reg.contracts.call(clist[0]);
+        }).then(function(contractRet){
+            console.log(contractRet);
+            //le struct vengono ritornate come array 
+            assert.equal(contractRet[0], accounts[0], "owner errato");
+            assert.equal(contractRet[1], accounts[1], "farmer errato");
+            assert.equal(contractRet[2].c, 10, "duration errata");
+            //funziona bene ma la conversione ha qualche problema
+            // assert.equal(contractRet[3], web3.fromAscii('ciao1', 32), "ipfsAddress errato");
+        }).then(done).catch(done);
+    });
+});
